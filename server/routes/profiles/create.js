cbf8ce60d345ea3bf72febf04fa3b01e9ddfe490
Spinaldash <blueavenger@gmail.com>
2015-06-01 15:14:46 -0700

    fixing editprofile page

--- server/routes/profiles/create.js
+++ server/routes/profiles/create.js
@@ -11,23 +11,7 @@ module.exports = {
     request.payload.interests = request.payload.interests.split(/\s*,\s*/);
     var profile = new Profile(request.payload);
     profile.save(function() {
-      Autocomplete.findOne({name: 'auto'}, function(err, auto) {
-        if (!auto) {
-          var newAuto = new Autocomplete({skills: profile.skills});
-          newAuto.save(function() {
-            reply({profile: profile});
-          });
-        } else {
-          console.log(auto);
-          profile.skills.forEach(function(e) {
-            auto.skills.push(e.toLowerCase());
-          });
-          auto.skills = _.uniq(auto.skills);
-          auto.save(function() {
-            reply({profile: profile});
-          });
-        }
-      });
+      reply({profile: profile});
     });
   }
 };
--- server/routes/profiles/edit.js
+++ server/routes/profiles/edit.js
@@ -4,6 +4,19 @@ var Profile = require('../../models/profile');
 
 module.exports = {
   handler: function(request, reply){
+    if(Array.isArray(request.payload.skills)){
+      request.payload.skills = request.payload.skills.join()
+    }
+    if(Array.isArray(request.payload.exposed)){
+      request.payload.exposed = request.payload.exposed.join()
+    }
+    if(Array.isArray(request.payload.interests)){
+      request.payload.interests = request.payload.interests.join()
+    }
+
+    request.payload.skills = request.payload.skills.split(/\s*,\s*/);
+    request.payload.exposed = request.payload.exposed.split(/\s*,\s*/);
+    request.payload.interests = request.payload.interests.split(/\s*,\s*/);
     Profile.findOneAndUpdate({_id: request.payload._id}, request.payload, function(err, profile) {
       if (err) { console.log(err); reply().code(400); }
       else { reply({profile: profile}); }
